93657d4f86a74529e9cf87c47c305e29c8585824
Ivan <vanechka-nikitin-2004@mail.ru>
2024-08-16 14:44:18 +0800

    Add logging tg commands

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-
 from aiogram import Bot, Dispatcher, types
+from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.utils import executor
 
 API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
@@ -8,6 +8,14 @@ CMC_API_KEY = os.getenv("CMC_API_KEY")
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
+
+
+class LoggingMiddleware(BaseMiddleware):
+    async def on_pre_process_message(self, message: types.Message, data: dict):
+        print(f"Received a message: {message.text}")
+
+
+dp.middleware.setup(LoggingMiddleware())
 
 
 @dp.message_handler(commands=['start'])
