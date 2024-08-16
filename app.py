51076eb2e42ee32326b24c83e5fecbc282b2569e
Ivan <vanechka-nikitin-2004@mail.ru>
2024-08-16 14:48:09 +0800

    Add tg command /set

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ class LoggingMiddleware(BaseMiddleware):
 
 
 dp.middleware.setup(LoggingMiddleware())
+tracked_currencies = {}
 
 
 def get_crypto_price(crypto, currency="USD"):
@@ -38,6 +39,18 @@ def get_crypto_price(crypto, currency="USD"):
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
     await message.reply("Привет! Я могу отслеживать цены на криптовалюты для тебя. Используй команду /set для задания валюты и пороговых значений.")
+
+
+@dp.message_handler(commands=['set'])
+async def set_currency(message: types.Message):
+    args = message.text.split()
+    if len(args) != 4:
+        await message.reply("Используйте команду в формате: /set <currency> <min_threshold> <max_threshold>")
+        return
+    currency, min_threshold, max_threshold = args[1].upper(), float(args[2]), float(args[3])
+    chat_id = message.chat.id
+    tracked_currencies[currency] = (min_threshold, max_threshold, chat_id)
+    await message.reply(f"Отслеживание {currency} с минимальным порогом {min_threshold} и максимальным порогом {max_threshold} установлено.")
 
 
 if __name__ == '__main__':
