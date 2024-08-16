28c9bd4951289d479eb7b0b2522dcea19a85f08d
Ivan <vanechka-nikitin-2004@mail.ru>
2024-08-16 14:42:34 +0800

    Add start command

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+CMC_API_KEY = os.getenv("CMC_API_KEY")
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Привет! Я могу отслеживать цены на криптовалюты для тебя. Используй команду /set для задания валюты и пороговых значений.")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
