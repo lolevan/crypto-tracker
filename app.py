c10d8505b0b52428e7486c812bb501b0bab0092a
Ivan <vanechka-nikitin-2004@mail.ru>
2024-08-16 14:51:33 +0800

    Fix schedule check

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ async def track_prices():
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.create_task(track_prices())
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, loop=loop, skip_updates=True)
